393d0c9c3f43a3351c80f34d15688af1dbd20b02
Joshua Choi <jsc.r839@gmail.com>
2019-06-08 20:38:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,9 @@
 var playlist = {"Anointing": "Our God"};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, { [artistName]: songTitle })
+  Object.assign({}, playlist, { [artistName]: songTitle })
+  playlist[artistName] = songTitle
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName){
